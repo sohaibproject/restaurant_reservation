d3eda14785e9c8f1a5adec425a7b3ad26091747f
boukraa sohaib <73255434+sohaibproject@users.noreply.github.com>
2024-08-20 00:33:33 +0100

    install the app && add typescript and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header bg-blue-500 text-white p-4'>
+        <h1 className='text-2xl font-bold'>Hello, Tailwind CSS!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
