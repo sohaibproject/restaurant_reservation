76fa2de262e633545a6394bb4ea5131ffc008a1a
boukraa sohaib <73255434+sohaibproject@users.noreply.github.com>
2024-08-21 02:56:21 +0100

    Update index.tsx

--- src/views/pages/reservationApp/index.tsx
+++ src/views/pages/reservationApp/index.tsx
@@ -7,29 +7,6 @@ import NavBar from '../../layout/navBar';
 
 const ReservationApp = () => {
   const [filteredReservations, setFilteredReservations] = useState<Reservation[]>(reservationData.reservations);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchReservations = async () => {
-      try {
-        // Fetch the local JSON file
-        const response = await fetch('../../../@core/data/mockData'); // Path to your local file
-        if (!response.ok) {
-          throw new Error('Failed to fetch reservations');
-        }
-        const data = await response.json();
-        // setFilteredReservations(data.reservations);
-        console.log(data);
-      } catch (err) {
-        // setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchReservations();
-  }, []);
-
   return (
     <>
       <NavBar />
